1f10017a650670e5771c6097075f8d7640ff4fc7
tloeber <thomas.loeber73@gmail.com>
2022-10-09 12:40:03 -0400

    Break up email functionality into separate modules.

---
+++ email_utils/email_labels.py
@@ -0,0 +1,19 @@
+import email_utils.gmail_client as client
+
+def list_labels():
+    gmail = client.create_client()
+    results = gmail.users().labels() \
+        .list(userId='me') \
+        .execute()
+
+    # TODO: Handle errors from gmail API If using this function in non-interactive way!
+    labels = results.get('labels', [])
+    
+    if not labels:
+        print('No labels found.')
+        return
+    
+    print('Labels and their IDs:')
+    for label in labels:
+        print(f"{label['name']} : {label['id']}")
+    return labels
--- get_emails.py
+++ get_emails.py
@@ -1,72 +1,13 @@
-from __future__ import print_function
-
-import os.path
 from typing import List
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
-
-
-def create_gmail_client():
-    creds = _authenticate()
-
-    # create gmail api client
-    client = build('gmail', 'v1', credentials=creds)
-    return client
-
-
-def _authenticate():
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    return creds
-
-
-def list_labels():
-    gmail_client = create_gmail_client()
-    results = gmail_client.users().labels() \
-        .list(userId='me') \
-        .execute()
-
-    # TODO: Handle errors from gmail API If using this function in non-interactive way!
-    labels = results.get('labels', [])
-    
-    if not labels:
-        print('No labels found.')
-        return
-    
-    print('Labels and their IDs:')
-    for label in labels:
-        print(f"{label['name']} : {label['id']}")
-    return labels
+import email_utils.gmail_client as client
+import email_utils.email_labels as labels
 
 
 def _list_threads_by_page(next_page_token=None):
-    gmail_client = create_gmail_client()
+    gmail = client.create_client()
 
-    response = gmail_client.users().threads() \
+    response = gmail.users().threads() \
         .list(userId='me', maxResults=500, pageToken=next_page_token) \
         .execute()
         
@@ -89,7 +30,7 @@ def list_all_threads() -> List:
 
 
 def main():
-    # list_labels()
+    labels.list_labels()
     threads = list_all_threads()
     # for thread in threads:
     #     print(thread)
