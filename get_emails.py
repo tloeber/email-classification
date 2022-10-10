4c5c3c33543dda333e67be687f231c365c615aa7
tloeber <thomas.loeber73@gmail.com>
2022-10-10 10:41:18 -0400

    Add function to retrieve details of a thread.

--- get_emails.py
+++ get_emails.py
@@ -4,18 +4,20 @@ from __future__ import annotations
 import email_utils.gmail_client as client
 import email_utils.email_labels as labels
 from data_models import Message 
-
+from domain_models import EmailThread
     
-def _list_threads_by_page(next_page_token=None) -> tuple[list, str]:
+    
+def _list_threads_by_page(next_page_token=None) -> tuple[list[str], str]:
     gmail = client.create_client()
 
     response = gmail.users().threads() \
         .list(userId='me', maxResults=500, pageToken=next_page_token) \
         .execute()
         
-    threads = response.get('threads', [])
+    # Only keep id of each thread (for retrieving thread details later)
+    threads = response.get('threads', []) \
+        .get['id']
     next_page_token = response.get('nextPageToken')
-
     return threads, next_page_token
 
 
@@ -29,6 +31,15 @@ def list_all_threads() -> list:
         # Exit loop once no more pages are left
         if not next_page_token:
             return all_threads
+
+
+def _get_thread_details(thread_id: str) -> EmailThread:
+    gmail = client.create_client()
+
+    thread = gmail.users().threads() \
+        .get(userId='me', id=thread_id) \
+        .execute()
+    return thread
 
 
 def main():
