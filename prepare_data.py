7558dc2dcdbc4724b38166e89a2db9a3bdf5995e
tloeber <thomas.loeber73@gmail.com>
2022-10-11 06:11:20 -0400

    Add counter for logging, saving, etc.

--- prepare_data.py
+++ prepare_data.py
@@ -5,14 +5,23 @@ import pickle
 
 from thread_api import list_all_thread_ids, assign_msg_labels
 
-PERSIST_RESULTS = False
-FILTER_QUERY = 'After:2022/10/01'
+PERSIST_RESULTS = True
+FILTER_QUERY = 'After:2022/01/01'
+
+class Counter:
+    def __init__(self):
+        self.state = 0
+
+    def increment(self):
+        self.state += 1
+
 
 def main():
     thread_ids = list_all_thread_ids(
         query=FILTER_QUERY
     )
-    msgs_replied_to, msgs_to_discard = assign_msg_labels(thread_ids=thread_ids)
+    msgs_replied_to, msgs_to_discard, dlq = \
+        assign_msg_labels(thread_ids=thread_ids)
     
     if PERSIST_RESULTS:
         with open('msgs_replied_to.pickle', 'wb') as f:
@@ -23,7 +32,8 @@ def main():
             
     print(
         len(msgs_replied_to),
-        len(msgs_to_discard)
+        len(msgs_to_discard),
+        dlq
     )
 
 if __name__ == '__main__':
