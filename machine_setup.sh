88312945a10d740dc591988fffecb72b89d604a9
tloeber <thomas.loeber73@gmail.com>
2022-10-18 12:51:10 +0000

    Start tracking setup script for sagemaker studio instances.

---
+++ machine_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Settings to adjust:
+# ===================
+GIT_REPO_URL="https://github.com/tloeber/email-classification.git"
+
+
+# GitHub
+# ======
+git config --global user.name tloeber
+git config --global user.email thomas.loeber73@gmail.com
+cd ~ && \
+    git clone $GIT_REPO_URL
+
+git config credential.helper 'cache --timeout=604800'
+
+# Bash aliases
+# ============
+wget https://raw.githubusercontent.com/tloeber/utils_and_configs/main/dotfiles/bash_aliases \
+    -O ~/.bash_aliases
+# There are no dotfiles in ~ (i.e., /root/) to modify, so use the ones in /etc
+echo "source ~/.bash_aliases" >> /etc/bash.bashrc
