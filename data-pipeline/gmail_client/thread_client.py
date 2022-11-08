3a11903ea7db55ed3789396830a98cb94a78feae
tloeber <thomas.loeber73@gmail.com>
2022-11-07 20:25:11 -0500

    Change thread client to  return raw response.
    Parsing will happen in domain object.

--- data-pipeline/gmail_client/thread_client.py
+++ data-pipeline/gmail_client/thread_client.py
@@ -1,58 +1,42 @@
+"""
+This module provides a wrapper around the gmail API's thread functionalities. It
+deals with raw data as they are returned by the API. Thus, it uses pydantic data
+classes to model *data structures* that validate and parse the input, rather 
+than dealing with traditional classes that primarily expose their behavior 
+rather than their data.
+ - and does not carry out
+any transformations. Instead, the latter is handled by the DataTransformer 
+class, which models the domain of email data for our ML use case.
+"""
+
 import logging
 
 from .core_client import CoreClient
-from ..data_schemas.gmail_api import Message, NextPageToken, ThreadId, MessageId
-from ..email_domain.thread import EmailThread
+from data_schemas.gmail_api import (
+    ThreadId, NextPageToken,
+    RawThreadSummary, RawThreadsList, 
+)
 
 
 logger = logging.getLogger(__name__)
 
 class ThreadClient:
+    """
+    API Documentation: 
+    https://googleapis.github.io/google-api-python-client/docs/dyn/gmail_v1.users.threads.html
+    """
     
-    def __init__(self, core_client: CoreClient) -> None:
+    def __init__(self) -> None:
+        # ThreadClient is *composed with* CoreClient
         self.core_client = CoreClient()
 
-    def get_thread_with_details(
-        self, thread_id: ThreadId
-    ) -> tuple[EmailThread, list[dict]]:
-        """
-        Given a thread id, create list of detailed EmailThread objects.
-        """
-
-        response = self.__get_thread_client() \
+    def get_thread(self, thread_id: ThreadId) -> RawThreadSummary:
+        response = self.__get_authenticated_thread_client() \
             .get(userId='me', id=thread_id) \
             .execute()
-        
-        # Validate message schema and drop fields not needed by converting each
-        # message to a pydantic data object.
-        msgs = []
-        dlq = []
+        return response
 
-        for msg in response['messages']:        
-            try:
-                valid_msg = Message(
-                    msg_id=msg['id'],
-                    sender=_get_sender(msg),
-                    body=_get_body_as_text(msg),
-                    timestamp=int(msg['internalDate'],)
-                )
-                msgs.append(valid_msg)
-
-            # If validation fails, save to dead letter queue
-            except Exception as e:
-                problem_details = {
-                    'exception': e,
-                    'message': msg
-                }
-                dlq.append(problem_details)
-
-        thread = EmailThread(
-            thread_id=thread_id, 
-            messages=msgs,
-        )
-        return thread, dlq
-
-    def list_all_thread_ids(self, query: str | None = None) -> list[ThreadId]:
+    def list_thread_ids(self, query: str | None = None) -> list[ThreadId]:
         """
         Get list of thread ids for a custom query, e.g. by date range. If more
         than one page is returned, this handels pagination under the hood.
@@ -60,25 +44,37 @@ class ThreadClient:
         all_thread_ids = []
         next_page_token = None
         while True:
-            thread_ids, next_page_token = self._list_thread_ids_paginated(
+            list_threads_response: RawThreadsList = self._list_thread_ids_paginated(
                 next_page_token=next_page_token, 
                 query=query
             )
+
+            # Only keep id of each thread (for retrieving thread details later)
+            try:
+                thread_ids = [
+                    thread['id'] for thread in list_threads_response['threads']
+                ]
+            except AttributeError:
+                thread_ids = []
             all_thread_ids.extend(thread_ids)
+            next_page_token = list_threads_response.get('nextPageToken')
 
             # Exit loop once no more pages are left
             if not next_page_token:
                 return all_thread_ids
 
-    def _list_thread_ids_paginated(
+    def list_thread_ids_raw(
         self,
-        next_page_token: NextPageToken | None = None,
         query: str | None = None,
-    ) -> tuple[list[ThreadId], NextPageToken]:
-        """Get list of thread ids for a single page"""
+        next_page_token: NextPageToken | None = None,
+    ) -> RawThreadsList:
+        """
+        Wrapper around Gmail API, returning response for listing thread ids for 
+        a single page.
+        """
 
         logger.info('Listing threadIDs for a single page.')        
-        response =  self.__get_thread_client() \
+        response =  self.__get_authenticated_thread_client() \
             .list(
                 userId='me', 
                 maxResults=500,
@@ -87,13 +83,8 @@ class ThreadClient:
                 pageToken=next_page_token, 
             ) \
             .execute()
-            
-        # Only keep id of each thread (for retrieving thread details later)
-        threads = response.get('threads', [])
-        thread_ids = [thread['id'] for thread in threads]
-        next_page_token = response.get('nextPageToken')
-        return thread_ids, next_page_token
-    
-    def __get_thread_client(self):
-        gmail_client = self.gmail_client.get_authenticated_client()
+        return response
+                    
+    def __get_authenticated_thread_client(self):
+        gmail_client = self.core_client.get_authenticated_client()
         return gmail_client.users().threads()
--- data-pipeline/test/utils/get_test_data.py
+++ data-pipeline/test/utils/get_test_data.py
@@ -5,12 +5,13 @@ import logging
 import sys
 import json
 
-# Add package root directory to path
+# Add package root directory to path. 
+# todo: Find better solution.
 package_root_dir = Path(__file__).parent.parent.parent.resolve()
 sys.path.append(
     str(package_root_dir)
 )
-from gmail_client.core_client import CoreClient
+from gmail_client.thread_client import ThreadClient
 
 
 PERSIST_RESULTS = True
@@ -20,46 +21,42 @@ THREAD_IDS = ['1838fb7ffac8ba90', '1838e67f60aea615', '1838aa69129f1f74']
 logger = logging.getLogger(__name__)
 
 
-def download_list_threads_response(filter_query, dir: Path):
-    """Call gmail API to list threads, and save response for unit testing."""
-    response =  thread_client.list(
-        userId='me', 
-        maxResults=500,
-        includeSpamTrash=False, 
-        q=filter_query,
-    ) \
-    .execute()
+class TestDataDownloader:
 
-    filename = test_dir / 'list_threads_result.json'
+    def __init__(self, download_dir: Path) -> None:
+        self.thread_client = ThreadClient()
+        self.download_dir = download_dir
 
-    with filename.open('w') as file:
-        json.dump(obj=response, fp=file)
+    def download_list_threads_response(self, filter_query):
+        """
+        Call gmail API to *list* threads, and save response for unit testing.
+        """
+        response =  self.thread_client \
+            .list_thread_ids_raw(query=filter_query, next_page_token=None)
 
-    print(f"Saved list-threads response to {filename}.")
+        file_path = self.download_dir / 'list_threads_result.json'
+        with open(file_path, 'w') as file:
+            json.dump(obj=response, fp=file)
+        print(f"Saved list-threads response to {self.download_dir}.")
 
-
-def download_get_thread_response(thread_id, dir: Path):
-    response = thread_client.get(
-        userId='me', 
-        id=thread_id
-    ) \
-    .execute()
+    def download_get_thread_response(self, thread_id):
+        response = self.thread_client.get_thread(thread_id=thread_id)
         
-    filename = dir / f'get_thread_{thread_id}.json'
+        file_path = self.download_dir / f'get_thread_{thread_id}.json'
+        with open(file_path, 'w') as file:
+            json.dump(obj=response, fp=file)
 
-    with filename.open('w') as file:
-        json.dump(obj=response, fp=file)
-
-    print(f"Saved list-threads response to {filename}.")
+        print(f"Saved list-threads response to {file_path}.")
 
 
 if __name__ == '__main__':
-    gmail_client = CoreClient().get_authenticated_client()
-    thread_client = gmail_client.users().threads()
-
     test_dir = Path(__file__).parent.parent.resolve()
     test_data_dir = test_dir / 'data'
 
-    download_list_threads_response(FILTER_QUERY, dir=test_data_dir)
+    downloader = TestDataDownloader(
+        download_dir=test_data_dir,
+        )
+
+    # downloader.download_list_threads_response(FILTER_QUERY, dir=test_data_dir)
     for thread_id in THREAD_IDS:
-        download_get_thread_response(thread_id, dir=test_dir)
+        downloader.download_get_thread_response(thread_id)
